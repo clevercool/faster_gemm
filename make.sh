06578fb6e130f98faad835c7b05a1b53f0129594
mivenHan <zhaohan_miven@sjtu.edu.cn>
2021-01-28 22:21:01 +0800

    update code

--- make.sh
+++ make.sh
@@ -2,5 +2,6 @@
 # nvcc -o tzgemm_out -arch=sm_75 -lcublas -lcurand --ptxas-options=-v -Xptxas -dlcm=ca faster_gemm.cu
 # nvcc -o tzgemm_mix -arch=sm_75 -lcurand -lcublas --ptxas-options=-v -Xptxas -dlcm=ca tzgemm.cu
 nvcc -o sgemm_mix  -arch=sm_75 -lcurand --ptxas-options=-v -Xptxas -dlcm=ca mix_sgemm.cu
+nvcc -o cutcp_mix  -arch=sm_75 -lcurand --ptxas-options=-v -Xptxas -dlcm=ca mix_cutcp.cu
 # V100
 # nvcc -o  tzgemm_out -arch=sm_70 -lcublas faster_gemm.cu
