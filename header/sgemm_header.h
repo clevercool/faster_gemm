ef7e4ca30dd7b82f7b563d7eb74816d06544c869
mivenHan <zhaohan_miven@sjtu.edu.cn>
2021-01-28 21:53:04 +0800

    update sgemm code

---
+++ header/sgemm_header.h
@@ -0,0 +1,5 @@
+#define TILE_N 16
+#define TILE_TB_HEIGHT 8
+#define TILE_M (TILE_N * TILE_TB_HEIGHT)
+
+#define SGEMM_GRID_DIM 68 * 2
--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #2080ti
-nvcc -o tzgemm_out -arch=sm_75 -lcublas -lcurand --ptxas-options=-v -Xptxas -dlcm=ca faster_gemm.cu
-nvcc -o tzgemm_mix -arch=sm_75 -lcurand -lcublas --ptxas-options=-v -Xptxas -dlcm=ca tzgemm.cu
+# nvcc -o tzgemm_out -arch=sm_75 -lcublas -lcurand --ptxas-options=-v -Xptxas -dlcm=ca faster_gemm.cu
+# nvcc -o tzgemm_mix -arch=sm_75 -lcurand -lcublas --ptxas-options=-v -Xptxas -dlcm=ca tzgemm.cu
+nvcc -o sgemm_mix  -arch=sm_75 -lcurand --ptxas-options=-v -Xptxas -dlcm=ca mix_sgemm.cu
 # V100
 # nvcc -o  tzgemm_out -arch=sm_70 -lcublas faster_gemm.cu
